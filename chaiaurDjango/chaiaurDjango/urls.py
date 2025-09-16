b04b3ef0adab921541731d57bd56664a0b2ab38a
Yashpreet Singh <yashpreet1686@gmail.com>
2025-09-17 03:28:50 +0530

    tailwind

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('chai/', include('chai.urls'))
+    path('chai/', include('chai.urls')),
+
+    path('__reload__/', include("django_browser_reload.urls"))
 ]
