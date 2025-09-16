c1bcc9044022a0d540e2ed1214df8ccc8dc5a40f
Yashpreet Singh <yashpreet1686@gmail.com>
2025-09-16 23:24:15 +0530

    templating engine, load css, views, urls, and settings.py

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Hello world, you are at chai django home page")
+    # return HttpResponse("Hello world, you are at chai django home page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello world, you are at chai django about page")
