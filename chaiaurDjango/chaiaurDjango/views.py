97907f6a91b5ddc668e5e812c6b21175ae06562d
Yashpreet Singh <yashpreet1686@gmail.com>
2025-09-17 02:58:08 +0530

    new app, jinja2, templates

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello world, you are at chai django home page")
-    return render(request, 'website/index.html')
+    return render(request, 'website/home.html')
 
 def about(request):
-    return HttpResponse("Hello world, you are at chai django about page")
+    return render(request, 'website/about.html')
 
 
 def contact(request):
-    return HttpResponse("Hello world, you are at chai django contact page")
+    return render(request, 'website/contact.html')
