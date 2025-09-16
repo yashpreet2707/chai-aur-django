fa7ca1d3883b55a48ba13b3a07112a98ec9100cf
Yashpreet Singh <yashpreet1686@gmail.com>
2025-09-17 04:25:59 +0530

    models, fetch and show data, routes

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'MASALA'),
+        ('GR', 'GINGER'),
+        ('KW', 'KIWI'),
+        ('EL', 'ELAICHI'),
+        ('PL', 'PLAIN'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
+    description = models.TextField(default="")
+
+def __str__(self):
+    return self.name
--- chaiaurDjango/chai/urls.py
+++ chaiaurDjango/chai/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_chai, name='all_chai'),
+    path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
 ]
